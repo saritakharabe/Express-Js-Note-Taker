c7650e491adb07656c1e26f36ee5ea2b567d6903
Sarita Kharabe <98185682+saritakharabe@users.noreply.github.com>
2023-04-20 14:55:14 -0400

    Updated PORT

--- server.js
+++ server.js
@@ -1,62 +1,69 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const noteData = require("./db/db.json");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 
-//loading and routing path index.html 
+//loading and routing path index.html
 app.get("/", (res, req) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-//loading and routing path notes.html 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+//loading and routing path notes.html
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {throw err};
-      let notes = JSON.parse(data);
-      res.json(notes);
-    });
-  });
-
-//post request for adding review
-app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-    const { title, text } = req.body;
-    if (title && text) {
-      const newNote = {
-        title,
-        text,
-      };
-      fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          const notes = JSON.parse(data);
-          notes.push(newNote);
-          fs.writeFile('./db/db.json', JSON.stringify(notes, null, '\t'),
-            (error, data) => error ? console.error(error) : console.info('Notes updates Successfully!')
-          );
-        }
-      });
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-      res.json(response);
-    } else {
-      res.json(' post feedback error');
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      throw err;
     }
+    let notes = JSON.parse(data);
+    res.json(notes);
   });
+});
 
+//post request for adding notes
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile(
+          "./db/db.json",
+          JSON.stringify(notes, null, "\t"),
+          (error, data) =>
+            error
+              ? console.error(error)
+              : console.info("Notes updates Successfully!")
+        );
+      }
+    });
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+    res.json(response);
+  } else {
+    res.json(" post feedback error");
+  }
+});
+
+//listen PORT
 app.listen(PORT, () => {
   console.info(`Listening port of the app is http://localhost:${PORT}`);
 });
